ebfec91347875c713ccb6b24420bee2cb7e41b41
jaysa <jaysagarcia@berkeley.edu>
2024-01-04 04:12:21 -0800

    finished overview tabcontents, connected to sheets

--- scripts/charts.js
+++ scripts/charts.js
@@ -26,10 +26,25 @@ const doughnutLabel = {
 }
 
 function initCharts() {
+//  createChart("2425uResChart", "https://docs.google.com/spreadsheets/d/1kccrFJIMqa07EfZEFPi4obVsTSS6wp-UuKxbihcfwvc/edit?usp=sharing");
+//  createChart("2425uNonresChart", "https://docs.google.com/spreadsheets/d/1ES4vP4YhGP7g-A1MeS4kd4uujn5eoe6JzhfAC-a3Fek/edit#gid=0")
+//  createChart("2425gResChart", "https://docs.google.com/spreadsheets/d/107BADicIfIDD6cJNeeZJnl6mAPkm6ncTnIyG5JeCbcU/edit#gid=0")
+//  createChart("2425gNonresChart", "https://docs.google.com/spreadsheets/d/1IRg13R20UU_EglYtFXNfZdIKF2AFrqMopvKifFj-WgY/edit#gid=0")
+
   createChart("2324uResChart", "https://docs.google.com/spreadsheets/d/1kccrFJIMqa07EfZEFPi4obVsTSS6wp-UuKxbihcfwvc/edit?usp=sharing");
   createChart("2324uNonresChart", "https://docs.google.com/spreadsheets/d/1ES4vP4YhGP7g-A1MeS4kd4uujn5eoe6JzhfAC-a3Fek/edit#gid=0")
   createChart("2324gResChart", "https://docs.google.com/spreadsheets/d/107BADicIfIDD6cJNeeZJnl6mAPkm6ncTnIyG5JeCbcU/edit#gid=0")
   createChart("2324gNonresChart", "https://docs.google.com/spreadsheets/d/1IRg13R20UU_EglYtFXNfZdIKF2AFrqMopvKifFj-WgY/edit#gid=0")
+
+  createChart("2223uResChart", "https://docs.google.com/spreadsheets/d/1uYON6Ewurxd4WuZ_OEf66wlFOERedVqfqVh7VmBLUXA/edit?usp=sharing");
+  createChart("2223uNonresChart", "https://docs.google.com/spreadsheets/d/1-KyVnfnByIt5h98rLBl6v-1qrFONMSXrszZYARFI_Wg/edit#gid=0")
+  createChart("2223gResChart", "https://docs.google.com/spreadsheets/d/1jCiWs262W9fLqPmvabrhppRvnp6IeISYd0QzMOtTzTs/edit#gid=0")
+  createChart("2223gNonresChart", "https://docs.google.com/spreadsheets/d/1zIKkylUReefhAdkwbs-HYHmbfLEHAKxYc6sptINNiBs/edit#gid=0")
+
+  createChart("2122uResChart", "https://docs.google.com/spreadsheets/d/113ygoLveJM89_LBVWrkRmN1E5WChZMjQEeltobFdq-k/edit#gid=0");
+  createChart("2122uNonresChart", "https://docs.google.com/spreadsheets/d/1OKP9tfrRRbuj4YHyTEkJWQvUgB5WmFTpnSXUe2A5KxU/edit#gid=0")
+  createChart("2122gResChart", "https://docs.google.com/spreadsheets/d/1nlwWd4tdWJePAUf9DgeYOqwP5V4jCAXwD6FGWne6oqo/edit#gid=0")
+  createChart("2122gNonresChart", "https://docs.google.com/spreadsheets/d/1yQjguOiQHoKsJdkVvK5oT5IsMTyt3FaaG7SnYIowXK4/edit#gid=0")
 }
 
 function createChart(containerId, dataUrl) {
